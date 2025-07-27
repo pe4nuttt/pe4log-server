628c8aff4045fe2b01a5e6efe69300563d61cc5b
Anh Nguyễn <anhnguyen@Anhs-MacBook-Pro.local>
2025-07-27 18:39:27 +0700

    [feat] migration files

--- src/database/migrations/1753372545393-AddCreateColumnLoginAttempt.ts
+++ src/database/migrations/1753372545393-AddCreateColumnLoginAttempt.ts
@@ -1,14 +1,19 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class AddCreateColumnLoginAttempt1753372545393 implements MigrationInterface {
-    name = 'AddCreateColumnLoginAttempt1753372545393'
+export class AddCreateColumnLoginAttempt1753372545393
+  implements MigrationInterface
+{
+  name = 'AddCreateColumnLoginAttempt1753372545393';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`login_attempt\` ADD \`createdAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6)`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE \`login_attempt\` ADD \`createdAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6)`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`login_attempt\` DROP COLUMN \`createdAt\``);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE \`login_attempt\` DROP COLUMN \`createdAt\``,
+    );
+  }
 }
--- src/modules/comment-reactions/dto/update-comment-reaction.dto.ts
+++ src/modules/comment-reactions/dto/update-comment-reaction.dto.ts
@@ -1,4 +1,6 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateCommentReactionDto } from './create-comment-reaction.dto';
 
-export class UpdateCommentReactionDto extends PartialType(CreateCommentReactionDto) {}
+export class UpdateCommentReactionDto extends PartialType(
+  CreateCommentReactionDto,
+) {}
