a9dd40215ca627dcd1e237f0b7386813a06b6e2b
Anh Nguyễn <anhnguyen@Anhs-MacBook-Pro.local>
2025-08-09 12:54:21 +0700

    feat: app setting migration

--- src/database/migrations/1753890350463-AddAppSettingTable.ts
+++ src/database/migrations/1753890350463-AddAppSettingTable.ts
@@ -1,14 +1,15 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class AddAppSettingTable1753890350463 implements MigrationInterface {
-    name = 'AddAppSettingTable1753890350463'
+  name = 'AddAppSettingTable1753890350463';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE \`app_setting\` (\`key\` varchar(255) NOT NULL, \`value\` json NOT NULL, \`updatedAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), PRIMARY KEY (\`key\`)) ENGINE=InnoDB`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE \`app_setting\` (\`key\` varchar(255) NOT NULL, \`value\` json NOT NULL, \`updatedAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), PRIMARY KEY (\`key\`)) ENGINE=InnoDB`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP TABLE \`app_setting\``);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE \`app_setting\``);
+  }
 }
